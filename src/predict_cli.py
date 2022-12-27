74b02e7a7a3dc3025e1e70229bcbfc63af5429b8
zhangted <tedcbook@gmail.com>
2022-12-27 13:46:37 -0500

    add basic non-cli usage

---
+++ src/predict_cli.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, './src')
+
+import models
+from Helpers import *
+from CLIConfig import *
+
+def main():
+  config = create_CLI_config()
+  image_batch = create_image_batch(config)
+  image_batch.process_cli(config)
+
+if __name__ == '__main__':
+  main()
